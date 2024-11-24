1c11970deffcf345f47713b61a36e1fd30e841f5
coder9-shivam <shiv.coder9@gmail.com>
2024-11-24 15:17:51 +0530

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,24 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));  // Load cart from localStorage if it exists
+export let cart;  // Load cart from localStorage if it exists
 
-if (!cart) {
-  // If no cart in localStorage, use default items
-  cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-    deliveryOptionId: '1'
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-    deliveryOptionId: '2'
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    // If no cart in localStorage, use default items
+    cart = [{
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: '2'
+    }
+    ];
   }
-  ];
 }
 
 function saveToStorage() {
