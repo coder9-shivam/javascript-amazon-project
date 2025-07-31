edf8bc41dd90296c8479831bba7ecb7e2551ee27
coder9-shivam <shiv.coder9@gmail.com>
2025-07-31 17:01:46 +0530

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,49 +6,18 @@ import { loadCart } from "../data/cart.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
-Promise.all([
-  loadProductsFetch(),
-  new Promise((resolve) => {
-    loadCart(() => {
-      resolve('val2');
-    });
-  })
+async function loadPage() {
+  await loadProductsFetch();
 
-]).then((value) => {
-  console.log(value);
-  renderCheckoutHeader();
-  renderOrderSummary();
-  renderPaymentSummary();
-});
-
-/*
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve('val1');
-  });
-
-}).then((value) => {
-  console.log(value);
-  return new Promise((resolve) => {
+  await new Promise((resolve) => {
     loadCart(() => {
       resolve();
     });
   });
 
-}).then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
-});
-*/
+}
 
-
-/*
-loadProducts(() => {
-  loadCart(() => {
-    renderCheckoutHeader();
-    renderOrderSummary();
-    renderPaymentSummary();
-  })
-});
-*/
+loadPage();
